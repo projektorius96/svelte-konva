d93bbd6b56a1f77ca09e731c9c2e3c464c260be2
Lukas Gaucas <34185919+projektorius96@users.noreply.github.com>
2023-02-17 19:38:22 +0000

    cracked it, see desc.
    - README.md should come within next commit, just want to make sure I had a working draft remotely before anything happened;

---
+++ src/exports_main.js
@@ -0,0 +1,9 @@
+import SvelteKonva from './lib/components/kIndex';
+import { ComponentLinker } from './lib/components/utils/ComponentLinker';
+
+export default (function (){
+  return {
+      SvelteKonva,
+      ComponentLinker
+  }
+}).call()
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    outDir: resolve(__dirname, 'src/svelte-konva-temp/'),
+    lib: {
+      entry: resolve(__dirname, 'src/exports_main.js'),
+      formats: ['es'],
+      fileName: 'index'
+    }
+    // @https://rollupjs.org/configuration-options
+    // rollupOptions: {
+    //    /* DEV_NOTE # inject rollup configs directly as if using rollup rather than Vite */
+    // },
+  },
 })
